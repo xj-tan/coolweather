54504d6c921f2f5d74bdf83f904c5c4c399915f3
xj-tan <xj_tan@163.com>
2018-05-20 16:30:31 +0800

    Add UI to show weather

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by nsctetan on 2018/5/20.
+ */
+
+public class AQI {
+
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
